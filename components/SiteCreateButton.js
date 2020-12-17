6ed24fb07499c80ba27c7879fbf766b802aea8e2
David Richer <david.richer@autodesk.com>
2020-12-17 16:34:22 -0500

    Revert "Fixed the AWS build"
    This reverts commit 1adcf2adf39272e21f7dcce001cba27e549709bd.

--- components/SiteCreateButton.js
+++ components/SiteCreateButton.js
@@ -8,77 +8,74 @@ import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 
-// import Amplify, { API } from 'aws-amplify';
-// import awsconfig from '../../src/aws-exports';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from '../../src/aws-exports';
 
 
-// Amplify.configure(awsconfig);
+Amplify.configure(awsconfig);
 
-// function createSite(site_name, branch) {
-//   const apiName = 'sgtestapi';
-//   const path = '/site';
-//   const myInit = {
-//     body: {app_tag: branch, site_name: site_name}
-//   };
+function createSite(site_name, branch) {
+  const apiName = 'sgtestapi';
+  const path = '/site';
+  const myInit = {
+    body: {app_tag: branch, site_name: site_name}
+  };
 
-//   return API.post(apiName, path, myInit);
-// }
+  return API.post(apiName, path, myInit);
+}
 
 export default function SiteCreateButton() {
-  return (
-    <Button variant="contained">Creat Site</Button>
-  );
-    // const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-    // const handleClickOpen = () => {
-    //   setOpen(true);
-    // };
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
 
-    // const handleCreate = (thisForm) => {
-    //   createSite(document.getElementById("site_name").value, document.getElementById("branch").value)
-    //   setOpen(false);
-    // };
+    const handleCreate = (thisForm) => {
+      createSite(document.getElementById("site_name").value, document.getElementById("branch").value)
+      setOpen(false);
+    };
 
-    // const handleCancel = () => {
-    //   setOpen(false);
-    // };
+    const handleCancel = () => {
+      setOpen(false);
+    };
 
-    // return (
-    //   <div>
-    //     <Button variant="contained" onClick={handleClickOpen}>
-    //       Create Test Site
-    //     </Button>
-    //     <Dialog open={open} onClose={handleCancel} aria-labelledby="form-dialog-title">
-    //       <DialogTitle id="form-dialog-title">Create Test Site</DialogTitle>
-    //       <DialogContent>
-    //         <DialogContentText>
-    //           Create a new test site
-    //         </DialogContentText>
-    //         <TextField
-    //           autoFocus
-    //           margin="dense"
-    //           id="site_name"
-    //           label="Site Name"
-    //           type="text"
-    //           fullWidth
-    //         />
-    //         <TextField
-    //           margin="dense"
-    //           id="branch"
-    //           label="Branch"
-    //           type="text"
-    //           fullWidth
-    //         />
-    //       </DialogContent>
-    //       <DialogActions>
-    //         <Button onClick={handleCancel} color="primary">
-    //           Cancel
-    //         </Button>
-    //         <Button onClick={handleCreate} color="primary">
-    //           Create
-    //         </Button>
-    //       </DialogActions>
-    //     </Dialog>
-    //   </div>
-    // );
+    return (
+      <div>
+        <Button variant="contained" onClick={handleClickOpen}>
+          Create Test Site
+        </Button>
+        <Dialog open={open} onClose={handleCancel} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Create Test Site</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Create a new test site
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="site_name"
+              label="Site Name"
+              type="text"
+              fullWidth
+            />
+            <TextField
+              margin="dense"
+              id="branch"
+              label="Branch"
+              type="text"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCancel} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleCreate} color="primary">
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
   }
