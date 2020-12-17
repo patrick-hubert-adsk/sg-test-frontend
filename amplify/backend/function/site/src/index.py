d823a0c7b4439b0a293190e65577758ce3cad773
Kenny Kwan <37555130+KwanK1@users.noreply.github.com>
2020-12-17 10:10:17 -0500

    Add /site endpoint (#4)

--- amplify/backend/function/site/src/index.py
+++ amplify/backend/function/site/src/index.py
@@ -1,6 +1,53 @@
+"""Fetches all test sites """
+
+import json
+import logging
+import urllib3
+import boto3
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
 def handler(event, context):
-  print('received event:')
-  print(event)
-  return {
-    'message': 'Hello from your new Amplify Python lambda!'
-  }
+    """
+    Fetch all test sites
+    """
+    try:
+        body = get_test_sites()
+        status_code = 200
+    except Exception as err:
+        logging.error("Unexpected error: %s", err)
+        body = {
+            "message": "Unexpected error"
+        }
+        status_code = 500
+    return {
+        "statusCode": status_code,
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": json.dumps(body)
+    }
+
+def get_test_sites():
+    """
+    Gets all test sites
+
+    Returns:
+        test_sites (List): List of active test sites
+    """
+    all_test_sites = []
+    client = boto3.client('cloudformation')
+    response = client.describe_stacks()
+    while len(response['Stacks']) > 0:
+        for stack in response['Stacks']:
+            if stack['StackName'].startswith('precit-app'):
+                # Converting datetime object to string
+                stack['CreationTime'] = str(stack['CreationTime'])
+                all_test_sites.append(stack)
+        if not response.get('NextToken'):
+            break
+        response = client.describe_stacks(
+            NextToken=response['NextToken']
+        )
+    return all_test_sites
